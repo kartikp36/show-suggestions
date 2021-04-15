1fa6abad271932c0821a5b4960092b4050dada2e
kartikp36 <kartikp2002@gmail.com>
2021-04-15 18:44:27 +0530

    fix: remove static builds from embed

--- pages/embed/[siteId].js
+++ pages/embed/[siteId].js
@@ -1,81 +1,76 @@
-import { useRef, useState } from 'react';
+import React, { useRef } from 'react';
 import { useRouter } from 'next/router';
 import PropTypes from 'prop-types';
+import useSWR, { mutate } from 'swr';
 import { Button } from '@chakra-ui/button';
 import { FormControl, FormLabel } from '@chakra-ui/form-control';
 import { Input } from '@chakra-ui/input';
 import { Box } from '@chakra-ui/layout';
 
 import Feedback from '../../components/Feedback';
-import { getActiveFeedback, getAllSites } from '../../lib/db-admin';
+import DashboardShell from '../../components/DashboardShell';
 import { createFeedback } from '../../lib/database';
 import { useAuth } from '../../lib/auth';
+import fetcher from '../../utils/fetcher';
 
-export async function getStaticProps(context) {
-  const siteId = context.params.siteId;
-  const { feedback } = await getActiveFeedback(siteId);
-  return {
-    props: {
-      initialFeedback: feedback,
-    },
-    revalidate: 1,
-  };
-}
-
-export async function getStaticPaths() {
-  const { sites } = await getAllSites();
-  const paths = sites.map((site) => ({
-    params: { siteId: site.id.toString() },
-  }));
-
-  return {
-    paths,
-    fallback: true,
-  };
-}
-
-const SiteFeedback = ({ initialFeedback }) => {
-  const auth = useAuth();
-  const router = useRouter();
+const SiteFeedback = () => {
+  const { user } = useAuth();
   const inputElement = useRef(null);
-  const [allFeedback, setAllFeedback] = useState(initialFeedback);
+  const router = useRouter();
+  const siteId = router.query?.siteId;
+  const { data: siteData } = useSWR(`/api/site/${siteId}`, fetcher);
+  const { data: feedbackData } = useSWR(
+    user ? [`/api/feedback/active/${siteId}`, user?.token] : null,
+    fetcher
+  );
+  const site = siteData?.site;
+  const allFeedback = feedbackData?.feedback;
 
   const handleSubmit = (e) => {
     e.preventDefault();
     const newFeedback = {
-      author: auth.user.name,
-      authorId: auth.user.uid,
+      siteAuthorId: site.authorId,
+      author: user.name,
+      authorId: user.uid,
       siteId: router.query.siteId,
       text: inputElement.current.value,
       createdAt: new Date().toISOString(),
-      provider: auth.user.provider,
-      status: 'pending',
+      provider: user.provider,
+      status: 'active',
     };
-    inputElement.current.value = '';
-    setAllFeedback([newFeedback, ...allFeedback]);
     createFeedback(newFeedback);
+    inputElement.current.value = '';
+    mutate(
+      [`/api/feedback/active/${siteId}`, user.token],
+      async (data) => ({
+        feedback: [newFeedback, ...data.feedback],
+      }),
+      false
+    );
   };
   return (
-    <Box display="flex" flexDirection="column" width="full">
-      <Box as="form" onSubmit={handleSubmit}>
-        <FormControl my={8}>
-          <FormLabel htmlFor="comment">Comment</FormLabel>
-          <Input
-            bgColor="white"
-            ref={inputElement}
-            type="comment"
-            id="comment"
-          />
-          <Button mt={2} type="submit" fontWeight="medium">
-            Add Comment
-          </Button>
-        </FormControl>
+    <DashboardShell>
+      <Box display="flex" flexDirection="column" width="full">
+        <Box as="form" onSubmit={handleSubmit}>
+          <FormControl my={8}>
+            <FormLabel htmlFor="comment">Comment</FormLabel>
+            <Input
+              bgColor="white"
+              ref={inputElement}
+              type="comment"
+              id="comment"
+            />
+            <Button mt={2} type="submit" fontWeight="medium">
+              Add Comment
+            </Button>
+          </FormControl>
+        </Box>
+        {allFeedback &&
+          allFeedback.map((feedback) => (
+            <Feedback key={feedback.id} {...feedback} />
+          ))}
       </Box>
-      {allFeedback &&
-        allFeedback.map((feedback) => (
-          <Feedback key={feedback.id} {...feedback} />
-        ))}
-    </Box>
+    </DashboardShell>
   );
 };
 export default SiteFeedback;
