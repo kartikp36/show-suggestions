d93981d3d4ee17df983ee7e7ae351e97c908db79
kartikp36 <kartikp2002@gmail.com>
2021-05-14 01:59:36 +0530

    fix: show feedbacks of the current user only

--- pages/api/feedback/[siteId].js
+++ pages/api/feedback/[siteId].js
@@ -1,9 +1,11 @@
-import { getAllSiteFeedback, getSite } from '../../../lib/db-admin';
+import { getUserSiteFeedback, getSite } from '../../../lib/db-admin';
+import { auth } from '../../../lib/firebase-admin';
 
 export default async (req, res) => {
   const siteId = req.query.siteId;
+  const { uid } = await auth.verifyIdToken(req.headers.token);
 
-  const { feedback, error } = await getAllSiteFeedback(siteId);
+  const { feedback, error } = await getUserSiteFeedback(siteId, uid);
   const { site } = await getSite(siteId);
   if (error) {
     res, status(500).json({ error });
